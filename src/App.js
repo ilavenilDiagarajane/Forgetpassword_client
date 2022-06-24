9b7f9c4aa0507bc9251f1ae00f6a6392cc820c1b
ilavenilDiagarajane <venirajane@gmail.com>
2022-06-24 15:21:58 +0530

    Forgetpassword_client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Forgotpassword from './Forgotpassword'
+import Resetpassword from './Resetpassword'
+import Msg from './Msg'
+import {Routes,Route,} from "react-router-dom";
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="/" element={<Forgotpassword />} />
+      <Route path="/resetpassword/:pass_token" element={<Resetpassword />} />
+      <Route path="/msg" element={<Msg />} />
+      </Routes>
+    
+
+    <ToastContainer autoclose={7000} />
     </div>
+    
   );
 }
 
